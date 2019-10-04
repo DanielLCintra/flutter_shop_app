0f10f1b92698395953636cdf677538b7f28a8a21
Daniel Cintra <cintra.70@gmail.com>
2019-10-04 11:18:27 -0300

    Implementação do provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_shop_app/screens/product_detail_screen.dart';
-import 'package:flutter_shop_app/screens/products_overview_screen.dart';
+import 'package:provider/provider.dart';
+import './screens/product_detail_screen.dart';
+import './screens/products_overview_screen.dart';
+import './providers/products_provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'MyShop',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.purple,
-          accentColor: Colors.deepOrange,
-          fontFamily: 'Lato',
-        ),
-        home: ProductsOverviewScreen(),
-        routes: {
-          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-        });
+    return ChangeNotifierProvider(
+      builder: (ctx) => ProductsProvider(),
+      child: MaterialApp(
+          title: 'MyShop',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primarySwatch: Colors.purple,
+            accentColor: Colors.deepOrange,
+            fontFamily: 'Lato',
+          ),
+          home: ProductsOverviewScreen(),
+          routes: {
+            ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+          }),
+    );
   }
 }
